82128feb678e9867c0fc3022e06041696ed0b1cb
Talha2299 <71260619+Talha2299@users.noreply.github.com>
2021-02-14 00:03:10 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,17 +7,14 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
-## Azure resource group for the kubernetes cluster ##
 resource "azurerm_resource_group" "aks_demo" {
   name     = var.resource_group_name
   location = var.location
 }
 
-## AKS kubernetes cluster ##
 resource "azurerm_kubernetes_cluster" "aks_demo" { 
   name                = var.cluster_name
   resource_group_name = azurerm_resource_group.aks_demo.name
@@ -27,7 +24,6 @@ resource "azurerm_kubernetes_cluster" "aks_demo" {
   linux_profile {
     admin_username = var.admin_username
 
-    ## SSH key is generated using "tls_private_key" resource
     ssh_key {
       key_data = "${trimspace(tls_private_key.key.public_key_openssh)} ${var.admin_username}@azure.com"
     }
@@ -48,12 +44,10 @@ resource "azurerm_kubernetes_cluster" "aks_demo" {
   }
 }
 
-## Private key for the kubernetes cluster ##
 resource "tls_private_key" "key" {
   algorithm   = "RSA"
 }
 
-## Save the private key in the local workspace ##
 resource "null_resource" "save-key" {
   triggers = {
     key = tls_private_key.key.private_key_pem
@@ -68,29 +62,8 @@ EOF
   }
 }
 
-## Outputs ##
 
-# Example attributes available for output
 output "id" {
     value = azurerm_kubernetes_cluster.aks_demo.id
 }
 
-output "client_key" {
-  value = azurerm_kubernetes_cluster.aks_demo.kube_config.0.client_key
-}
-
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks_demo.kube_config.0.client_certificate
-}
-
-output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.aks_demo.kube_config.0.cluster_ca_certificate
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks_demo.kube_config_raw
-}
-
-output "host" {
-  value = azurerm_kubernetes_cluster.aks_demo.kube_config.0.host
-}
